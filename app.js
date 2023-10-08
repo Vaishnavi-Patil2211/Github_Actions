fbc9f8b401f1aadb0cce71b0aa260808b075c1c9
vaishnavi patil <vp397264@gmail.com>
2023-10-08 21:57:08 +0530

    practice

---
+++ app.js
@@ -0,0 +1,16 @@
+// app.js
+
+function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  const result1 = add(3, 2);
+  console.log(`3 + 2 = ${result1}`);
+  
+  const result2 = subtract(5, 2);
+  console.log(`5 - 2 = ${result2}`);
+  
